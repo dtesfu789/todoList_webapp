a1080bca02864765abcaae4010b1961e5a421e97
dtesfu789 <ddytesfu@gmail.com>
2022-05-24 11:25:19 +0200

    clean commit, remote db, remote deploy

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const myDate = require(__dirname+'/date.js');
+const mongoose = require('mongoose');
+const _ = require('lodash');
 const app = express();
 const port = 3000;
 
-let day = "";
-let taskInput = "";
-let taskList = ['task 01', 'task 02', 'task 03'];
-let workList = [];
-let houseWorkList = [];
+mongoose.connect('mongodb+srv://admin-dereje:wishDB@cluster0.6enrmrf.mongodb.net/todoeyDB');
+const TodoeySchema = new mongoose.Schema({
+  name: String
+});
+const Task = mongoose.model("Task", TodoeySchema);
+const task01 = new Task({
+  name: 'Welcome to your Todoey List!'
+});
+const task02 = new Task({
+  name: 'Hit the + button to add a new item.'
+});
+const task03 = new Task({
+  name: '<-- Hit this to delete an item.'
+});
 
+let taskList = [task01, task02, task03];
+
+const ListSchema = new mongoose.Schema({
+  name: String,
+  items: [TodoeySchema]
+});
+const List = mongoose.model("List", ListSchema);
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -17,43 +34,100 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.get("/", function(req, res) {
-  let today = myDate.getDay();
-  res.render('list', {
-    ListTitle: today,
-    UserTaskInput: taskList
+  Task.find({}, function(err, docs) {
+    if (docs.length === 0) {
+      Task.insertMany(taskList, function(err) {
+        if (err) {
+          console.log(err);
+        };
+      });
+
+      res.redirect("/");
+    } else {
+      res.render('list', {
+        ListTitle: "Today",
+        UserTaskInput: docs
+      });
+    }
+  });
+
+});
+
+app.get("/:customListName", function(req, res) {
+  const customListName = _.capitalize(Object.values(req.params)[0]);
+  const list = new List({
+    name: customListName,
+    items: taskList
+  });
+  List.findOne({
+    name: customListName
+  }, function(err, docs) {
+    if (!err) {
+      if (!docs) {
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        res.render('list', {
+          ListTitle: docs.name,
+          UserTaskInput: docs.items
+        })
+      }
+    }
   });
 });
+
 app.post("/", function(req, res) {
-  taskInput = req.body.task;
-  if (req.body.submit === "Work") {
-    workList.push(taskInput);
-    res.redirect("/work");
-  } else if (req.body.submit === "House Work") {
-    houseWorkList.push(taskInput);
-    res.redirect("/house_work")
-  } else {
-    taskList.push(taskInput);
-    res.redirect("/");
-  }
-
-
-});
-app.get("/work", function(req, res) {
-  res.render('list', {
-    ListTitle: "Work",
-    UserTaskInput: workList
+  customListName = req.body.submit;
+  const taskInput = new Task({
+    name: req.body.task
   });
-})
-app.get("/house_work", function(req, res) {
-  res.render('list', {
-    ListTitle: "House Work",
-    UserTaskInput: houseWorkList
-  })
+  if (req.body.submit === "Today") {
+    taskInput.save();
+    res.redirect("/");
+  } else {
+    List.findOne({
+      name: customListName
+    }, function(err, docs) {
+      docs.items.push(taskInput);
+      docs.save();
+      res.redirect("/" + customListName);
+    });
+  }
 });
 
-app.get("/about", function(req, res) {
-  res.render('about');
+app.post("/delete", function(req, res) {
+  const checkedBoxName = Object.keys(req.body);
+  const checkedBoxValue = Object.values(req.body)[0];
+  const pageIdentifier = checkedBoxName[0].split("_")[0];
+  const dtbID = checkedBoxName[0].split("_")[1];
+  if (checkedBoxValue === "on") {
+    if (pageIdentifier === "Today") {
+      Task.findByIdAndRemove(dtbID, function(err) {
+        console.log(err);
+      });
+      res.redirect("/");
+    } else {
+
+      List.findOneAndUpdate({
+        name: pageIdentifier
+      }, {
+        $pull: {
+          items: {
+            _id: dtbID
+          }
+        }
+      }, function(err, docs) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.redirect("/" + pageIdentifier);
+        }
+      });
+
+    }
+  }
 });
+
 
 app.listen(port, function() {
   console.log("server started at port 3000");
